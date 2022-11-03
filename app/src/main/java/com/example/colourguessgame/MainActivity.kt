ab4116db819bc671e8d644687fd7f95e64f51968
Shagun <shagunchahar8@gmail.com>
2022-11-03 18:27:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/colourguessgame/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.colourguessgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var b1 = findViewById<Button>(R.id.button)
+        var b2 = findViewById<Button>(R.id.button2)
+        var b3 = findViewById<Button>(R.id.button3)
+        var b4 = findViewById<Button>(R.id.button4)
+        var b5 = findViewById<Button>(R.id.button5)
+        var b6 = findViewById<Button>(R.id.button6)
+        var b7 = findViewById<Button>(R.id.button7)
+        var b8 = findViewById<Button>(R.id.button8)
+        var b9 = findViewById<Button>(R.id.button9)
+        var b10 = findViewById<Button>(R.id.button10)
+        var b11 = findViewById<Button>(R.id.button11)
+        var b12 = findViewById<Button>(R.id.button12)
+        var b13 = findViewById<Button>(R.id.button13)
+        var b14 = findViewById<Button>(R.id.button14)
+        
+
+    }
+}
